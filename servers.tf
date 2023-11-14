2f6ad692c3ccba04fc6c35b4103ce9290522bd28
aneef <awss2423@gmail.com>
2023-11-14 18:48:43 +0530

    roboshop-terraform-servers

--- servers.tf
+++ servers.tf
@@ -3,8 +3,8 @@ data "aws_ami" "centos" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
 }
-data "aws_security_group" "allow.all" {
-  name = "allow.all"
+#data "aws_security_group" "allow.all" {
+  #name = "allow.all"
 }
 
 resource "aws_instance" "frontend" {
